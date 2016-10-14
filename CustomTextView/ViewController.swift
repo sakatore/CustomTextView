f8cefcac655fc507a3cea3575f08542e01a742db
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2016-10-14 17:41:04 +0900

    Initial Commit

---
+++ CustomTextView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomTextView
+//
+//  Created by 酒井恭平 on 2016/10/14.
+//  Copyright © 2016年 酒井恭平. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

dd067f6b1f010cc8e4c9886b971f6db77087e112
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2016-10-21 22:19:17 +0900

    Add placeholder in TextView

--- CustomTextView/CustomTextView.swift
+++ CustomTextView/CustomTextView.swift
@@ -9,14 +9,65 @@
 import UIKit
 
 class CustomTextView: UITextView {
+    private let placeholderLabel: UILabel = UILabel()
     
+    // default is nil. string is drawn 70% gray
+    var placeholder: String? {
+        didSet {
+            print("placeholder did set.")
+            drawPlaceholder(in: self.frame)
+        }
+    }
     
-    /*
+    func drawPlaceholder(in rect: CGRect) {
+        // 通知を登録する
+        NotificationCenter.default.addObserver(self, selector: #selector(CustomTextView.controlPlaceholder(_:)), name: .UITextViewTextDidChange, object: nil)
+    }
+    
+    //  TextViewのTextが変更された時に呼ばれる
+    func controlPlaceholder(_ notification: NSNotification) {
+        print("UITextViewTextDidChange!")
+        placeholderIsHidden()
+        
+    }
+    
+    private func placeholderIsHidden() {
+        if text.isEmpty {
+            placeholderLabel.isHidden = false
+        } else {
+            placeholderLabel.isHidden = true
+        }
+    }
+    
+    override var text: String! {
+        didSet {
+            print("didiSet: " + text)
+            placeholderIsHidden()
+        }
+    }
+    
+    override var textAlignment: NSTextAlignment {
+        didSet {
+            print("didiSet: \(textAlignment)")
+        }
+    }
+    
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        super.draw(rect)
+        
+        placeholderLabel.frame = rect
+        placeholderLabel.text = placeholder
+        placeholderLabel.font = self.font
+        placeholderLabel.backgroundColor = UIColor.clear
+        placeholderLabel.textColor = UIColor.gray.withAlphaComponent(0.7)
+        placeholderLabel.textAlignment = self.textAlignment
+        placeholderLabel.sizeToFit()
+        
+        self.addSubview(placeholderLabel)
+        self.sendSubview(toBack: placeholderLabel)
+        print("Add placeholderLabel as subView")
     }
-    */
 
 }
--- CustomTextView/ViewController.swift
+++ CustomTextView/ViewController.swift
@@ -9,17 +9,70 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var textView: CustomTextView = CustomTextView()
+    var textField: UITextField = UITextField()
+    
+    var width: CGFloat = 200
+    var textViewHeight: CGFloat = 100
+    var textFieldhHeight: CGFloat = 50
+    var buttonHeight: CGFloat = 50
+    var posX: CGFloat { return self.view.frame.width / 2}
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        configTextView()
+        configTextField()
+        configButton()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func configTextView() {
+        textView.frame.size = CGSize(width: width, height: textViewHeight)
+        textView.layer.position = CGPoint(x: posX, y: 100)
+        textView.text = "Fist text"
+        textView.placeholder = "Placeholder"
+        
+        self.view.addSubview(textView)
     }
-
+    
+    func configTextField() {
+        textField.frame.size = CGSize(width: width, height: textFieldhHeight)
+        textField.layer.position = CGPoint(x: posX, y: 100 + textViewHeight / 2 + textViewHeight / 2)
+        textField.text = "Fist text"
+        textField.placeholder = "Placeholder"
+        textField.borderStyle = .line
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.textFieldTextDidChanged(_:)), name: .UITextFieldTextDidChange, object: nil)
+        
+        self.view.addSubview(textField)
+    }
+    
+    func textFieldTextDidChanged(_ notification: NSNotification) {
+        print("UITextFieldTextDidChange!")
+    }
+    
+    func configButton() {
+        let button = UIButton(type: .system)
+        button.frame.size = CGSize(width: width, height: buttonHeight)
+        button.layer.position = CGPoint(x: posX, y: textField.layer.position.y + textFieldhHeight / 2 + buttonHeight / 2)
+        button.setTitle("Button", for: .normal)
+        button.setTitleColor(UIColor.red, for: .normal)
+        button.setTitle("Pushed", for: .highlighted)
+        button.setTitleColor(UIColor.blue, for: .highlighted)
+        button.addTarget(self, action: #selector(ViewController.tapButton(_:)), for: .touchUpInside)
+        
+        self.view.addSubview(button)
+    }
+    
+    func tapButton(_ sender: UIButton) {
+        print("TapButton!")
+        textView.text = textView.text + "+addText"
+        //        textView.textAlignment = .center
+        textField.text = textField.text! + "+addText"
+    }
 
 }
 

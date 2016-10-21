93db40005c3bdf4b0b6e6be7573a0a6aaa14987c
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2016-10-21 22:14:34 +0900

    Add CustomTextView.swift

---
+++ CustomTextView/CustomTextView.swift
@@ -0,0 +1,22 @@
+//
+//  CustomTextView.swift
+//  CustomTextView
+//
+//  Created by 酒井恭平 on 2016/10/21.
+//  Copyright © 2016年 酒井恭平. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextView: UITextView {
+    
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
